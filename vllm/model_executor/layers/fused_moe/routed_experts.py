refactor: optimize LK_THREADS calculation and use cfg.use_gpu_prefill in lk-moe v2.3.4 fix https://github.com/guqiong96/Lsglang/issues/16#issuecomment-5297172594: add NUMA interleave override  when LVLLM_MOE_NUMA_ENABLED=1 and LVLLM_ENABLE_NUMA_INTERLEAVE=1

vllm/model_executor/layers/fused_moe/routed_experts.py
@@ -151,6 +151,7 @@ class RoutedExperts(PluggableLayer):
             )
         self.max_num_batched_tokens = vllm_config.scheduler_config.max_num_batched_tokens
         self.max_num_group_batch_size = self.get_max_num_group_batch_size()
+        self.use_gpu_prefill = is_lk_moe_use_gpu_prefill()
         
         from vllm.model_executor.layers.fused_moe.config import MoEActivation
         
@@ -1533,6 +1534,7 @@ class RoutedExperts(PluggableLayer):
             self.lk_moe_config.swiglu_alpha = self.swiglu_alpha 
         if self.swiglu_limit is not None:
             self.lk_moe_config.swiglu_limit = self.swiglu_limit
+        self.lk_moe_config.use_gpu_prefill = self.use_gpu_prefill
 
         # no global scale
         if self.params_dtype == torch.bfloat16:
@@ -1615,6 +1617,7 @@ class RoutedExperts(PluggableLayer):
             self.lk_moe_config.swiglu_alpha = self.swiglu_alpha 
         if self.swiglu_limit is not None:
             self.lk_moe_config.swiglu_limit = self.swiglu_limit
+        self.lk_moe_config.use_gpu_prefill = self.use_gpu_prefill
 
         # no global scale
         if self.params_dtype == torch.bfloat16:
@@ -1666,7 +1669,7 @@ class RoutedExperts(PluggableLayer):
             self.lk_moe_config.swiglu_alpha = self.swiglu_alpha 
         if self.swiglu_limit is not None:
             self.lk_moe_config.swiglu_limit = self.swiglu_limit
-        
+        self.lk_moe_config.use_gpu_prefill = self.use_gpu_prefill
         # no scale
         if self.params_dtype == torch.bfloat16:
             self.lk_moe = lk_moe.MOE_BF16(
@@ -1739,6 +1742,7 @@ class RoutedExperts(PluggableLayer):
             self.lk_moe_config.swiglu_alpha = self.swiglu_alpha 
         if self.swiglu_limit is not None:
             self.lk_moe_config.swiglu_limit = self.swiglu_limit
+        self.lk_moe_config.use_gpu_prefill = self.use_gpu_prefill
         
          
         if self.params_dtype == torch.bfloat16:
@@ -1801,6 +1805,7 @@ class RoutedExperts(PluggableLayer):
             self.lk_moe_config.swiglu_alpha = self.swiglu_alpha 
         if self.swiglu_limit is not None:
             self.lk_moe_config.swiglu_limit = self.swiglu_limit
+        self.lk_moe_config.use_gpu_prefill = self.use_gpu_prefill
 
         # no global scale
         if self.params_dtype == torch.bfloat16: